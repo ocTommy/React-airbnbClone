19659e1ac25e2504ad83d1c1432296bf07eb49a3
Tommy Nguyen <Tommy.nguyenoc@gmail.com>
2023-09-12 19:40:41 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import { Navbar } from './components/Navbar'
+import './style.css'
+
+export function App() {
+
+  return (
+    <>
+      <Navbar/> 
+    </>
+  )
+}
+
+
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,8 @@
+export const Navbar = () => {
+    return (
+        <nav>
+           <img src="src/assets/logo.png" alt="airbnb logo"/>
+        </nav>
+    );
+}
+ 
